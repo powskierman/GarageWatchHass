1948841efec2be9325a976911d702d55fc435b6e
powskierman <mlapoint99@hotmail.com>
2024-01-23 11:43:19 -1000

    Initial Commit

---
+++ GarageWatchHass Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GarageWatchHass Watch App
+//
+//  Created by Michel Lapointe on 2024-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GarageWatchHass Watch App/GarageWatchHassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarageWatchHassApp.swift
+//  GarageWatchHass Watch App
+//
+//  Created by Michel Lapointe on 2024-01-23.
+//
+
+import SwiftUI
+
+@main
+struct GarageWatchHass_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
